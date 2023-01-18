bf8a88d4ff12a69ed1838bf5afe305fb1cbe76d4
ChristophertheDeveloper <38364484+ChristophertheDeveloper@users.noreply.github.com>
2023-01-17 20:04:15 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    const btn = document.getElementById('btn');
+
+    btn.addEventListener('click', function(){
+        let weight = document.getElementById('weight-input').value;
+        let height = document.getElementById('height-input').value;
+        let finalbmi = (weight / (height * height) * 10000);
+        document.getElementById('bmi-output').value = finalbmi;
+    })
+    
+     });
+  
